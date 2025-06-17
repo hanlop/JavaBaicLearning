3c5627fb5a5d714058389100d3f96b9f1e9c7c3c
HanLop <112152150+hanlop@users.noreply.github.com>
2025-06-17 15:04:37 +0800

    Add files via upload
    Java入门

---
+++ Day01/src/com/oracle/Demo01.java
@@ -0,0 +1,25 @@
+package com.oracle;
+
+public class Demo01 {
+    //编写一个 描述手机品牌的信息的
+    //并将信息进行打印
+    //品牌 brand
+    //类型 type
+    //内存 memory
+    //颜色 color
+
+    public static void main(String[] args) {
+        String brand="苹果";
+        String type="iPhone 15 pro";
+        int memory=256;
+        String color="星空色";
+        double price=4999.0;
+
+        System.out.println("品牌: "+brand);
+        System.out.println("类型: "+type);
+        System.out.println("内存: "+memory);
+        System.out.println("颜色: "+color);
+        System.out.println("价格: "+price);
+
+    }
+}
---
+++ Day01/src/com/oracle/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.oracle;
+
+// 这是java中 类的基本格式
+public class HelloWorld {
+    /*
+    main方法时函数的入口
+     */
+    public static void main(String[] args) {
+        //这是 输出语句
+              System.out.println("Hello Java");
+        System.out.println("Hello Java");
+    }
+}
